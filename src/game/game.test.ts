90fb701d231e48bd66ad3a430d030cb41aecadfa
Vahid <kheradmand.v1@gmail.com>
2021-08-18 15:12:47 +0200

    Start to add game logic

---
+++ src/game/game.test.ts
@@ -0,0 +1,11 @@
+describe('Game', () => {
+  //beforeEach(() => {});
+
+       describe('can create a new game by getting the size of dimension and player count', () => {
+
+  });
+
+    describe('can accept moves', () => {
+        
+    })
+});
