c1608fb2b10d3d54e2aef4dcf6887d62840dd9e4
Vahid <kheradmand.v1@gmail.com>
2021-08-18 16:07:17 +0200

    Add some tests

--- src/game/game.test.ts
+++ src/game/game.test.ts
@@ -1,11 +1,30 @@
-describe('Game', () => {
-  //beforeEach(() => {});
+import { Chance } from 'chance';
+import { Game} from './game';
 
-       describe('can create a new game by getting the size of dimension and player count', () => {
+describe('Game', () =>        {
+  const chance = Chance();
 
+  describe('can create a new game by getting the size of dimension and player count', () => {
+
+
+    it.each([{
+      players: [chance.guid(), chance.guid()],
+      size: 3
+    }])('can create a game with 2 players', ({ size, players }) => {
+      const newGame = new Game({size, players });
+      
+      expect(newGame.size).toBe(size);
+      expect(newGame.playerCount).toBe(players.length);
+      expect(newGame.nextPlayer).toBe(players[0]);
+    });
   });
 
-    describe('can accept moves', () => {
-        
-    })
+  // describe('can accept moves', () => {
+  //
+  //   expect(true).toBe(false);
+  //
+  // });
 });
+
+         
+         
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,3 +1,15 @@
-export class Game {
+interface GameConstructorInput {
+  size: number
+  players: string[]
+}
 
+export class Game {
+  // private size: number
+  // private players: string[]
+  //
+  //
+  // constructor({ size, players }: GameConstructorInput) {
+  //   this.size = size;
+  //   this.players = players;
+  // }
 }
