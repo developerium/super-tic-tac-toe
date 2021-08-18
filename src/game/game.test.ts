6f0ee795b39594347976d0214af28724137c710f
Vahid <kheradmand.v1@gmail.com>
2021-08-18 20:41:40 +0200

    Add tests for game's constructor

--- src/game/game.test.ts
+++ src/game/game.test.ts
@@ -1,22 +1,33 @@
 import { Chance } from 'chance';
-import { Game} from './game';
+import { Game } from './game';
 
-describe('Game', () =>        {
+describe('Game', () => {
   const chance = Chance();
 
-  describe('can create a new game by getting the size of dimension and player count', () => {
+  describe('constructor', () => {
+    it.each([
+      {
+        players: Array.from(new Array(2), () => chance.guid()),
+        size: 3,
+      },
+      {
+        players: Array.from(new Array(3), () => chance.guid()),
+        size: 9,
+      },
+    ])(
+      'can create a new game by getting the size of dimension and players',
+      ({ size, players }) => {
+        const newGame = new Game({ size, players });
 
+        expect(newGame.playerCount).toBe(players.length);
+        expect(newGame.nextPlayer).toBe(players[0]);
 
-    it.each([{
-      players: [chance.guid(), chance.guid()],
-      size: 3
-    }])('can create a game with 2 players', ({ size, players }) => {
-      const newGame = new Game({size, players });
-      
-      expect(newGame.size).toBe(size);
-      expect(newGame.playerCount).toBe(players.length);
-      expect(newGame.nextPlayer).toBe(players[0]);
-    });
+        const tiles = newGame.getTiles();
+
+        expect(tiles.length).toBe(size);
+        expect(tiles[0].length).toBe(size);
+      }
+    );
   });
 
   // describe('can accept moves', () => {
@@ -25,6 +36,3 @@ describe('Game', () =>        {
   //
   // });
 });
-
-         
-         
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,15 +1,29 @@
+import { generateBySize, TileRow } from './tile/tile';
+
 interface GameConstructorInput {
-  size: number
-  players: string[]
+  size: number;
+  players: string[];
 }
 
 export class Game {
-  // private size: number
-  // private players: string[]
-  //
-  //
-  // constructor({ size, players }: GameConstructorInput) {
-  //   this.size = size;
-  //   this.players = players;
-  // }
+  private readonly players: string[];
+  private playerIndex = 0;
+  private tiles: TileRow[];
+
+  get playerCount(): number {
+    return this.players.length;
+  }
+
+  get nextPlayer(): string {
+    return this.players[this.playerIndex];
+  }
+
+  constructor({ size, players }: GameConstructorInput) {
+    this.players = players;
+    this.tiles = generateBySize(size);
+  }
+
+  getTiles() {
+    return this.tiles;
+  }
 }
