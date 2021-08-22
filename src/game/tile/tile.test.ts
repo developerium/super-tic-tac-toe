ad95f9f96e31e83eebadcf0eee773079d4889a1f
Vahid <kheradmand.v1@gmail.com>
2021-08-22 11:57:09 +0200

    Find a winner in a tileset

--- src/game/tile/tile.test.ts
+++ src/game/tile/tile.test.ts
@@ -1,8 +1,7 @@
-import { generateBySize } from './tile';
+import { Chance } from 'chance';
+import { generateBySize, getWinner, Pin, TileRow, WinnerResult } from './tile';
 
 describe('Tile', () => {
-  //beforeEach(() => {});
-
   describe('generateBySize', () => {
     it('can generate a 3x3 empty array', () => {
       expect(generateBySize(3)).toStrictEqual([
@@ -36,5 +35,31 @@ describe('Tile', () => {
         [null, null, null, null, null, null, null, null, null, null],
       ]);
     });
+  });
+});
+
+describe('getWinner', () => {
+  const chance = Chance();
+
+  it('getWinnerHorizontally', () => {
+    const players = Array.from(new Array(2), () => chance.guid());
+    const tiles = [
+      [{ player: players[1], pin: Pin.Medium }, null, null],
+      [
+        { player: players[0], pin: Pin.Medium },
+        { player: players[0], pin: Pin.Small },
+        { player: players[0], pin: Pin.Large },
+      ],
+      [null, null, { player: players[1], pin: Pin.Medium }],
+    ] as TileRow[];
+
+    expect(getWinner(tiles)).toStrictEqual({
+      player: players[0],
+      locations: [
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+      ],
+    } as WinnerResult);
   });
 });
--- src/game/tile/tile.ts
+++ src/game/tile/tile.ts
@@ -11,6 +11,66 @@ export interface Tile {
 
 export type TileRow = (Tile | null)[];
 
+export interface Location {
+  x: number;
+  y: number;
+}
+
+export interface WinnerResult {
+  player: string;
+  locations: Location[];
+}
+
 export const generateBySize = (size: number): TileRow[] => {
   return Array.from(new Array(size), () => new Array(size).fill(null));
+};
+
+export const getWinner = (tiles: TileRow[]): WinnerResult | null => {
+  // const length = tiles.length;
+  // for (let i = 0; i < length; i++) {
+  //   const tileRow = tiles[i];
+  //
+  //   for (let j = 0; j < length; j++) {
+  //     const tile = tiles[i][j];
+  //   }
+  // }
+
+  return getWinnerHorizontally(tiles);
+};
+
+const getWinnerHorizontally = (tiles: TileRow[]): WinnerResult | null => {
+  const length = tiles.length;
+
+  let player: string | null = null;
+  let locations: Location[] = [];
+
+  for (let x = 0; x < length; x++) {
+    const firstTile = tiles[x][0];
+    const lastTile = tiles[x][length - 1];
+
+    if (!firstTile || !lastTile) {
+      continue;
+    }
+
+    player = firstTile.player;
+    locations.push({ x, y: 0 });
+
+    for (let y = 1; y < length; y++) {
+      const tile = tiles[x][y];
+
+      if (player !== tile?.player) {
+        player = null;
+        locations = [];
+        break;
+      }
+
+      locations.push({ x, y });
+    }
+
+    if (player) {
+      return { player, locations };
+    }
+  }
+
+  return null;
 };
