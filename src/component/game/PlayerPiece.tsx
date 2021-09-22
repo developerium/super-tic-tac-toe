0c0ecb96849139cadc136c855f0d62839898a2e4
Vahid <kheradmand.v1@gmail.com>
2021-09-22 22:23:21 +0200

    Refactor player piece

--- src/component/game/PlayerPiece.tsx
+++ src/component/game/PlayerPiece.tsx
@@ -9,49 +9,42 @@ interface PlayerPieceProps {
   player: Player;
 }
 
+interface PieceProps {
+  draggableId: string;
+}
+
 const Large = styled.h3``;
 const Medium = styled.h4``;
 const Small = styled.h5``;
 
+const Piece: FC<PieceProps> = ({ children, draggableId }) => (
+  <Draggable draggableId={draggableId} index={0}>
+    {(provided, snapshot) => (
+      <div
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+      >
+        {children}
+      </div>
+    )}
+  </Draggable>
+);
+
 export const PlayerPiece: FC<PlayerPieceProps> = ({ player }) => {
   return (
     <div className={`nes-text ${player.cssClass}`}>
-      <Draggable draggableId="draggable-1" index={0}>
-        {(provided, snapshot) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <Large>large: {player.pieces[Pin.Large]}</Large>
-          </div>
-        )}
-      </Draggable>
+      <Piece draggableId="draggable-1">
+        <Large>large: {player.pieces[Pin.Large]}</Large>
+      </Piece>
 
-      <Draggable draggableId="draggable-2" index={0}>
-        {(provided, snapshot) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <Medium>medium: {player.pieces[Pin.Medium]}</Medium>
-          </div>
-        )}
-      </Draggable>
+      <Piece draggableId="draggable-2">
+        <Medium>medium: {player.pieces[Pin.Medium]}</Medium>
+      </Piece>
 
-
-      <Draggable draggableId="draggable-3" index={0}>
-        {(provided, snapshot) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <Small>small: {player.pieces[Pin.Small]}</Small>
-          </div>
-        )}
-      </Draggable>
+      <Piece draggableId="draggable-3">
+        <Small>small: {player.pieces[Pin.Small]}</Small>
+      </Piece>
     </div>
   );
 };
